55b6e1fdbb59f4722ffd17d03522d1f4086aaa93
vtecx <32472356+vtecx@users.noreply.github.com>
2017-10-03 00:20:47 -0400

    Create Bootstrap Image Popup.js

---
+++ JS/Bootstrap Image Popup.js
@@ -0,0 +1,10 @@
+//<![CDATA[
+$(window).load(function(){
+$("#pop").on("click", function(e) {
+   e.preventDefault();
+   $('#the-modal').modal('toggle');
+});
+});
+//]]> 
+
+// This .js file was created by PI (tjxpi.com). Copyright 2017 PI. All rights reserved.
